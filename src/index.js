ced0f71f0faebae779b6d00860f54bf33ed63e71
ЯRаСлав <support@yaro.page>
2025-08-18 01:43:16 +0300

    exporting more classes.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import LogConsole from "./Console.js"
 import Logger from "./Logger.js"
+import LoggerFormat from "./LoggerFormat.js"
+import NoConsole from "./NoConsole.js"
 import NoLogger from "./NoLogger.js"
 
 export {
+	LogConsole,
 	Logger,
+	LoggerFormat,
+	NoConsole,
 	NoLogger,
 }
 
